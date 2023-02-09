65693032c901b7add3a34f789783bc385b94fd63
Humberto Vallejo <111450057+humbertovacu@users.noreply.github.com>
2023-02-09 19:23:09 +0100

    block model created

---
+++ models/Block.model.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+const blockSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+
+    description: {
+      type: String,
+      required: true
+    },
+
+    steps: {
+      type: [{ type : Schema.Types.ObjectId, ref: 'Journey' }],
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    importance:{
+        type: String,
+        required: true,
+        enum:["Recommended", "Critical", "Optional"]
+    },
+  }, 
+  {
+    timestamps: true,
+  }
+);
+
+const Block = model("Block", blockSchema);
+
+module.exports = Block;
