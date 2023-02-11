a221a7d25cf03ffa1b1b488c816f100545021a3a
Humberto Vallejo <111450057+humbertovacu@users.noreply.github.com>
2023-02-11 10:30:23 +0100

    PUT /:journeyId route added and tested

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -2,6 +2,66 @@ const express = require("express");
 const router = express.Router();
 const Step = require("../models/Step.model");
 const Block = require("../models/Block.model")
+const Journey = require("../models/Journey.model")
+const fileUploader = require("../config/cloudinary.config");
+
+
+router.post('/journeys', (req, res) => {
+    
+    const { title, description, author, tags, isPublic } = req.body;
+    let image = '';
+
+    if(title === '' || description ===  ''){
+        res.status(400).json({message: 'Please add a title and a description to your new journey'})
+    }
+
+    if(!req.file){
+        image = 'https://res.cloudinary.com/djwmauhbh/image/upload/v1676047808/journey-app-assets/journey-default_aay5tv.jpg'
+    } else {
+        image = req.file.path
+    }
+
+    Journey.create({title, description, author, image, tags, isPublic })
+        .then(createdJourney => {
+            res.status(201).json(createdJourney);
+        })
+        .catch(err => res.status(500).json({message: 'Internal Server Error. Please try again.'}))
+
+});
+
+router.get('/:journeyId', (req, res) => {
+    
+    const { journeyId } = req.params;
+
+    Journey.findById(journeyId)
+        .then(foundJourney => res.status(200).json(foundJourney))
+        .catch(err => res.status(404).json({message: `Sorry, we couldn't find this page.`}));
+
+});
+
+router.put('/:journeyId', async (req, res) =>  {
+
+    const { journeyId } = req.params;
+    const { title, description, tags, image, isPublic } = req.body;
+   
+    let userJourney = await Journey.findById(journeyId);
+    userJourney.tags.addToSet(tags);
+    await userJourney.save();
+    
+    Journey.findByIdAndUpdate(journeyId, {title, description, image, isPublic}, {new: true})
+        .then(updatedJourney => res.status(200).json(updatedJourney))
+        .catch(err => res.status(500).json({message: "Sorry, we couldn't update this journey."}))
+
+});
+
+router.post('/upload', fileUploader.single("imageUrl"), (req, res) => {
+    if(!req.file){
+        res.status(400).json({message : 'No file uploaded'})
+    }
+
+    res.json({imageUrl: req.file.path});
+
+});
 
 router.post('/steps', (req,res)=>{
     console.log(req.body)
@@ -16,7 +76,7 @@ router.post('/steps', (req,res)=>{
             //Missing: Push the newly created Step into the Block model (steps property) , we can 
             // find the ID of the model through the URL (useParams hook necessary)
         })
-  })
+});
 
 router.get('/steps/:stepsId', (req,res)=>{
     const stepId = req.params.stepsId
@@ -51,7 +111,7 @@ router.post('/:journeyId/blocks', async (req, res)=> {
     };
 
     Block.create({title, description, category, importance})
-        .then(createdBlock => {res.status(201).json(createdBlock.data)})
+        .then(createdBlock => res.status(201).json(createdBlock))
         //Logic for pushing createdBlock._id into Journey.blocks//;
         .catch(err => {
             res.status(500).json({message: "Internal server error. Please try again."})
