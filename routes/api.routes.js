fff7dceb14efc24285ad496e95222c679ad0f767
Paulfresnel <paul.fresnel1@gmail.com>
2023-02-16 17:21:10 +0100

    Updates Routes and secured mongoDB URL
    Routes functional on frontend +full DB Functionality

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -4,9 +4,22 @@ const Step = require("../models/Step.model");
 const Block = require("../models/Block.model");
 const Journey = require("../models/Journey.model");
 const fileUploader = require("../config/cloudinary.config");
+const User = require("../models/User.model");
 
 
-router.post(':userId/journeys', (req, res) => {
+router.get('/users/:userId', (req,res)=>{
+    const {userId} = req.params
+    console.log(userId)
+    User.findById(userId).populate('journeysCreated')
+        .then(response=>{
+            let {_id, username, email, journeysCreated, journeysCopied, journeysCompleted, profilePicture} = response
+            res.json({_id, username, email, journeysCreated, journeysCompleted, journeysCopied, profilePicture})
+    })
+        .catch(err=>console.log(err))
+})
+
+
+router.post('/:userId/journeys', async (req, res) => {
     
     const { title, description, tags, isPublic } = req.body;
     const { userId } = req.params;
@@ -22,19 +35,16 @@ router.post(':userId/journeys', (req, res) => {
         image = req.file.path
     }
 
-    Journey.create({title, description, author: userId, image, tags, isPublic })
-        .then(createdJourney => {
-            res.status(201).json(createdJourney);
-        })
-        .catch(err => res.status(500).json({message: 'Internal Server Error. Please try again.'}))
-
+  let createdJourney = await  Journey.create({title, description, author: userId, image, tags, isPublic }).catch(err=>console.log(err))
+  let updatedUser = await  User.findByIdAndUpdate(userId, {$push :{journeysCreated: createdJourney._id}}).populate("journeysCreated").catch(err=>console.log(err))
+    res.json({user: updatedUser})
 });
 
 router.get('/journeys/:journeyId', (req, res) => {
     
     const { journeyId } = req.params;
 
-    Journey.findById(journeyId)
+    Journey.findById(journeyId).populate('blocks')
         .then(foundJourney => res.status(200).json(foundJourney))
         .catch(err => res.status(404).json({message: `Sorry, we couldn't find this page.`}));
 
@@ -90,7 +100,7 @@ router.post('/:blockId/steps', async (req,res)=>{
     let stepCreated = await  Step.create({title, description, links, difficulty,importance, notes ,image})
     console.log(stepCreated)
 
-           await Block.findByIdAndUpdate(blockId, {$push: {steps: stepCreated._id}})
+           await Block.findByIdAndUpdate(blockId, {$push: {steps: stepCreated._id}}, {new:true}).populate('steps')
                 .then(blockResponse=>{
                     console.log("block response")
                     console.log(blockResponse)
@@ -136,7 +146,7 @@ router.delete('/steps/:stepsId', (req,res)=>{
 
 // Block Routes
 
-router.post(':journeyId/blocks', async (req, res)=> {
+router.post('/:journeyId/blocks', async (req, res)=> {
 
     const { title, description, category, importance } = req.body;
     const { journeyId } = req.params;
@@ -147,8 +157,8 @@ router.post(':journeyId/blocks', async (req, res)=> {
 
     Block.create({title, description, category, importance})
         .then(async createdBlock => {
-            await Journey.findByIdAndUpdate(journeyId, {$push: {blocks: createdBlock._id}})
-            res.status(201).json(createdBlock)})
+           let updatedJourney = await Journey.findByIdAndUpdate(journeyId, {$push: {blocks: createdBlock._id}}, {new: true}).populate('blocks')
+            res.status(201).json(updatedJourney)})
         .catch(err => {
             res.status(500).json({message: "Internal server error. Please try again."})
         });
@@ -176,7 +186,6 @@ router.get('/blocks/:blockId', (req, res) => {
     Block.findById(blockId)
         .populate('steps')
         .then(blockFound => {
-            console.log(blockFound);
             res.status(200).json({block: blockFound});
         })
         .catch(err => res.status(500).json({message: "Internal Server Error. Please try again."}))  
@@ -195,13 +204,28 @@ router.put('/blocks/:blockId/', (req, res) => {
 });
 
 
-router.delete('blocks/:blockId/', (req, res) => {
+router.delete('/:journeyId/blocks/:blockId/', async (req, res) => {
     
     const { blockId } = req.params;
+    const {journeyId} = req.params
 
-    Block.findByIdAndRemove(blockId)
-        .then(() => res.status(200).json({message: "Block deleted"}))
-        .catch(err => res.status(500).json({message: "Internal Server Error. Please try again."}))
+   await Block.findByIdAndDelete(blockId)
+        .then(async () => {
+            let newBlocks=[]
+            await Journey.findById(journeyId)
+                .then(async (journeyFound)=>{
+                  let blockToRemove = journeyFound.blocks.find(block=> block == blockId)
+                    journeyFound.blocks.splice(journeyFound.blocks.findIndex(block=>block===blockToRemove),1)
+                    await Journey.findByIdAndUpdate(journeyId, {blocks: journeyFound.blocks}, {new:true})
+                        .then(journeyUpdated=>{
+                            res.status(200).json({message: "Block deleted"})
+                        })
+                })
+            
+    })
+        .catch(err => {
+            res.status(500).json({message: "Internal Server Error. Please try again."})
+        })
 
 });
 
